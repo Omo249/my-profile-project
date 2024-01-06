9638ef27346acbe7c601e9763bfb2f49a6b355c0
Omo249 <omolade249ahmed@gmail.com>
2024-01-06 15:55:19 +0100

    corrected js with thirdpage

--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ var replacementOneDiv;
 var replacementTwoDiv;
 // script.js
 // script.js
-function toggleMenu(menuButton, popupMenu) {
-  if (popupMenu.style.display === "block") {
-    popupMenu.style.display = "none";
-  } else {
-    popupMenu.style.display = "block";
-  }
-}
-
-menuButton.addEventListener("click", function () {
-  toggleMenu(menuButton, popupMenu);
-});
 
 menuButtonTwo.addEventListener("click", function () {
   toggleMenu(menuButtonTwo, popupMenuTwo);
@@ -25,10 +14,6 @@ function toggleMenu(menuButton, popupMenu) {
     popupMenu.style.display = "block";
   }
 }
-
-menuButton.addEventListener("click", function () {
-  toggleMenu(menuButton, popupMenu);
-});
 
 menuButtonTwo.addEventListener("click", function () {
   toggleMenu(menuButtonTwo, popupMenuTwo);
@@ -50,7 +35,7 @@ window.onload = function () {
 
   const contentDiv = document.getElementById("row");
   const surveyDiv = document.getElementById("survey-disabled");
-  const thirdDiv = document.getElementById("sRow"); // Add this line
+  const thirdDiv = document.getElementById("sfirst"); // Add this line
 
   sneakernessButton.addEventListener("click", function () {
     contentDiv.style.display = "none";
@@ -65,40 +50,21 @@ window.onload = function () {
   });
 
   thirdButton.addEventListener("click", function () {
-    // Add this block
     contentDiv.style.display = "none";
     surveyDiv.style.display = "none";
     thirdDiv.style.display = "block"; // Show the third page when going to the Third Page
   });
 
-  const surveyElement = document.querySelector("#survey-disabled");
-  surveyElement.classList.add("initially-hidden");
-
   sneakernessButton.addEventListener("click", function () {
-    surveyElement.classList.remove("initially-hidden");
-  });
-
-  homeButton.addEventListener("click", function () {
-    surveyElement.classList.add("initially-hidden");
-  });
-  const thirdPageElement = document.getElementById("sfirst");
-
-  thirdPageElement.classList.add("initially-hidden");
-
-  // ...
-
-  thirdButton.addEventListener("click", function () {
     contentDiv.style.display = "none";
-    surveyDiv.style.display = "none";
-    thirdPageElement.style.display = "block"; // Show the third page when going to the Third Page
+    surveyDiv.style.display = "block";
+    thirdDiv.style.display = "none"; // Hide the third page when going to Sneakerness
   });
-
-  // ...
 
   homeButton.addEventListener("click", function () {
     contentDiv.style.display = "block";
     surveyDiv.style.display = "none";
-    thirdPageElement.style.display = "none"; // Hide the third page when going back to Home
+    thirdDiv.style.display = "none"; // Hide the third page when going back to Home
   });
 };
 
@@ -114,7 +80,7 @@ const myButtonSec = document.getElementById("reb");
 // Add a click event listener to the button
 myButtonSec.addEventListener("click", function () {
   // Show an alert when the button is clicked
-  alert("Email:r.ollivieira@berlagelyceum.eu" );
+  alert("Email:r.ollivieira@berlagelyceum.eu");
 });
 const myButtonThird = document.getElementById("school");
 
